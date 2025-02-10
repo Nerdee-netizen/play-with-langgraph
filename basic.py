b181cdff9c8b5f06e6e0f76375450ed6f71ba642
zz <zhengzhe0148@outlook.com>
2025-02-10 13:15:27 +0000

    start

---
+++ basic.py
@@ -0,0 +1,49 @@
+from typing import Annotated
+
+
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph
+from langgraph.graph.message import add_messages
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+
+
+def chatbot(state: State):
+    return {"messages": "hello world"}
+
+
+# The first argument is the unique node name
+# The second argument is the function or object that will be called whenever
+# the node is used.
+graph_builder.add_node("stupid", chatbot)
+graph_builder.set_entry_point("stupid")
+graph_builder.set_finish_point("stupid")
+graph = graph_builder.compile()
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value)
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+
+        stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
